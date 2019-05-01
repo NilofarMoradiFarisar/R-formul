0a407a4ee641006c3ae4e14239d3e8e3eab69da7
Nilofar <48630863+NilofarMoradiFarisar@users.noreply.github.com>
2019-05-01 17:46:04 +0430

    Add files via upload

---
+++ formul8.R
@@ -0,0 +1,21 @@
+
+ ####################------  Damage -------####################
+
+
+ d=read.table("3d.txt")
+
+	library("nlstools")
+
+		D=d$V2
+		W=d$V1
+		y=d$V3
+
+	formulaExp <- as.formula(y ~ a+(D^b)*(W^c))
+
+	s=nls(formulaExp, start = list(a = 2, b = 1.6, c = 1))
+		summary(s)
+			try(s)
+				overview(s)
+
+       yp=predict(s)
+
